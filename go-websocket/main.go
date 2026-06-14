4b7d2661a73762c9c24de02bddd7f7fdafe6f077
CursedByAMummy <nicklevitsky223@gmail.com>
2026-06-14 17:13:49 -0400

    whew boy, here's the first rendition of this and in despair

---
+++ go-websocket/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"sync"
+)
+	// Upgrader is used to upgrade HTTP connections to WebSocket connections.
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var (
+	players = make(map[string]*Player)
+	mutex 	sync.RWMutex
+)
+
+type Player struct {
+	ID string
+	Conn *websocket.Conn
+
+	X int
+	Y int
+}
+
+type ClientMessage struct {
+	Type		string `json:"type"`
+	Direction	string `json:"direction"`
+}
+
+type PlayerState struct {
+	ID string 	`json:"id"`
+	X int		`json:"x"`
+	Y int		`json:"y"`
+}
+
+type GameState struct {
+	Players []PlayerState `json:"players"`
+}
+
+func getGameState() GameState {
+	state := GameState{}
+
+	mutex.RLock()
+	defer mutex.RUnlock()
+
+	for _, p := range players {
+		state.Players = append(
+			state.Players,
+			PlayerState{
+				ID: p.ID,
+				X: p.X
+				Y: p.Y,
+			},
+		)
+	}
+	return state
+}
+
+func broadcastState() {
+	state := getGameState()
+
+	mutex.RLock()
+	defer mutex.RUnlock()
+
+	for _, player := range players {
+		player.Conn.WriteJSON(state)
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Upgrade the HTTP connection to a WebSocket connection
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading:", err)
+		return
+	}
+
+	mutex.RLock()
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		http.Error(w, "UserID is required", http.StatusBadRequest)
+		return
+	}
+	if len(players) >= 4 {
+		mutex.RUnlock()
+		http.Error(w, "Game Full", http.StatusForbidden)
+		return
+	}
+	id := fmt.Sprintf(
+		"player%d",
+		len(players)+1,
+	)
+
+	player := &Player{
+		ID:		id,
+		Username:	userID,
+		Conn:		conn,
+		X:		0,
+		Y:		0,
+	}
+	players[id] = player
+	mutex.RUnlock()
+
+	defer conn.Close()
+
+	go handleConnection(player)
+}
+
+func handleConnection(player *Player) {
+	conn := player.Conn
+	for {
+		var msg ClientMessage
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			break
+		}
+		switch msg.Type {
+		case "move":
+		}
+	}
+	fmt.Printf("Received: %s\n", message)
+	if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+		fmt.Println("Error writing message:", err)
+		break
+	}
+}
+														  }
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	fmt.Println("WebSocket server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
